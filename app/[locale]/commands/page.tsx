f9687277338ab8e5bd656f7b18e8e436fffb1d0d
Артем Апенко <lbapart@gmail.com>
2025-01-18 19:13:55 +0100

    added commands translation

--- app/[locale]/commands/page.tsx
+++ app/[locale]/commands/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { getMessages } from "next-intl/server";
 import { Messages } from "@/types/messages";
+import { useTranslations } from "next-intl";
 
 export async function generateMetadata({
   params: { locale },
@@ -31,28 +32,30 @@ export async function generateMetadata({
 }
 
 const commands = [
-  { id: "start", title: "/start", description: "Старт работы с Telegram ботом." },
-  { id: "update_api_keys", title: "/update_api_keys", description: "Позволяет обновить ключи API для подключения к бирже. Ключи обновяться только при смене всех трех значений на новые." },
-  { id: "get_api_details", title: "/get_api_details", description: "Показывает текущие ключи API, которые вы использовали для подключения." },
-  { id: "update_trade_settings", title: "/update_trade_settings", description: "Позволяет изменить настройки вашей торговой стратегии." },
-  { id: "get_trade_settings", title: "/get_trade_settings", description: "Отображает текущие параметры вашей торговли." },
-  { id: "cancel", title: "/cancel", description: "Отменяет текущее действие и возвращает вас в главное меню." },
-  { id: "get_subscription_status", title: "/get_subscription_status", description: "Показывает статус вашей подписки и дату её окончания." },
-  { id: "renew_subscription", title: "/renew_subscription", description: "Позволяет продлить подписку. Продление возможно не ранее чем за 3 недели до окончания текущей подписки." },
-  { id: "start_trading", title: "/start_trading", description: "Активирует торговлю на основе текущих настроек." },
-  { id: "stop_trading", title: "/stop_trading", description: "Останавливает торговлю." },
-  { id: "set_language", title: "/set_language", description: "Позволяет выбрать язык взаимодействия с ботом." },
-  { id: "buy_coin", title: "/buy_coin", description: "Выполняет покупку криптовалюты на определённую сумму." },
-  { id: "help", title: "/help", description: "Показывает контактные данные для связи с поддержкой." },
+  { id: "start", title: "/start", description: "start_description" },
+  { id: "update_api_keys", title: "/update_api_keys", description: "update_api_keys_description" },
+  { id: "get_api_details", title: "/get_api_details", description: "get_api_details_description" },
+  { id: "update_trade_settings", title: "/update_trade_settings", description: "update_trade_settings_description" },
+  { id: "get_trade_settings", title: "/get_trade_settings", description: "get_trade_settings_description" },
+  { id: "cancel", title: "/cancel", description: "cancel_description" },
+  { id: "get_subscription_status", title: "/get_subscription_status", description: "get_subscription_status_description" },
+  { id: "renew_subscription", title: "/renew_subscription", description: "renew_subscription_description" },
+  { id: "start_trading", title: "/start_trading", description: "start_trading_description" },
+  { id: "stop_trading", title: "/stop_trading", description: "stop_trading_description" },
+  { id: "set_language", title: "/set_language", description: "set_language_description" },
+  { id: "buy_coin", title: "/buy_coin", description: "buy_coin_description" },
+  { id: "help", title: "/help", description: "help_description" },
 ];
 
 export default function CommandsPage() {
+  const t = useTranslations("CommandsPage");
+
   return (
     <div className="flex">
       {/* Scroll Area */}
       <div className="hidden md:block fixed top-16 left-0 h-[calc(100vh-8rem)] w-64 bg-white border-none overflow-y-auto">
         <ScrollArea className="h-full flex flex-col items-center justify-start px-6 space-y-6">
-          <h2 className="text-lg text-gray-800 font-bold text-center">Команды</h2>
+          <h2 className="text-lg text-gray-800 font-bold text-center">{t("title")}</h2>
           <div className="flex flex-col w-full space-y-2">
             {commands.map((command) => (
               <Button asChild
@@ -79,7 +82,7 @@ export default function CommandsPage() {
               <CardTitle>{command.title}</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>{command.description}</p>
+              <p>{t(command.description)}</p>
               {/* Additional command details */}
               {command.id === "update_api_keys" && (
                 <ul className="mt-2 list-disc ml-6">
@@ -90,18 +93,18 @@ export default function CommandsPage() {
               )}
               {command.id === "buy_coin" && (
                 <span>
-                  <p className="mt-2">Бот запросит ввести сумму покупки в USDT.</p>
-                  <strong className="mt-2">Минимальное значение - 5</strong>
+                  <p className="mt-2">{t("buy_coin_description_additional")}</p>
+                  <strong className="mt-2">{t("minimal_value")} - 5</strong>
                 </span>
               )}
               {command.id === "update_trade_settings" && (
                 <ul className="mt-2 list-disc ml-6">
-                  <li><span className="italic font-bold">Текущий баланс </span>- сумма в USDT, которую бот может использовать для совершения сделок.</li>
-                  <li><p className="mt-2"><span className="italic font-bold">Процент прибыли </span>-<strong> Минимальное значение - 0.1, Максимальное значение - 99. </strong></p>Процент, который бот будет использовать для расчета цены продажи от цены покупки.
+                  <li><span className="italic font-bold">{t("current_balance")} </span>- {t("current_balance_description")}</li>
+                  <li><p className="mt-2"><span className="italic font-bold">{t("profit_percent")} </span>-<strong> {t("minimal_value")} - 0.1, {t("maximum_value")} - 99. </strong></p>{t("profit_percent_description")}
                   </li>
-                  <li><p className="mt-2"><span className="italic font-bold">Процент шага </span>-<strong> Минимальное значение - 0.1, Максимальное значение - 99. </strong></p>Процент, который бот будет использовать для расчета цены покупки от цены последней покупки/продажи.
+                  <li><p className="mt-2"><span className="italic font-bold">{t("step_percent")} </span>-<strong> {t("minimal_value")} - 0.1, {t("maximum_value")} - 99. </strong></p>{t("step_percent_description")}
                   </li>
-                  <li><p className="mt-2"><span className="italic font-bold">Сумма покупки </span>-<strong> Минимальное значение - 5. </strong></p>Сумма в USDT, которую бот будет использовать для покупки криптовалюты.
+                  <li><p className="mt-2"><span className="italic font-bold">{t("buy_amount")} </span>-<strong> {t("minimal_value")} - 5. </strong></p>{t("buy_amount_description")}
                   </li>
                 </ul>
               )}
